e1c30f72c1fa6a6eac0ab9ec1f50a80f6386f78b
Agaur28 <gauraditi876@gmail.com>
2025-06-14 18:03:19 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,24 @@
+// A simple HTTP server using Node.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Welcome to the Homepage</h1>');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>About Page</h1>');
+  } else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'This is a JSON response' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>404 Not Found</h1>');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
